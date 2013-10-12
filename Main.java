a81f122a651f35f25c1b8c7da49decbf229c4e2c
LucasOliveira94 <94lucasoliveira@bol.com.br>
2013-10-12 16:53:58 -0300

    Create Main.java
    Classe Main.java - Jogo da vida simples em java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * GITHUB - LUCASOLIVEIRA94
+ */
+
+public class Main{
+   
+   public static void main(String args[]){
+   
+       Janela janela = new Janela();
+       janela.executar();
+    
+   }
+    
+}
